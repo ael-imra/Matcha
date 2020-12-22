a3d2417e6e3e23de8b6be26628f341995c0f2aa3
Soufiane El hamri <sel-hamr@e2r12p1.1337.ma>
2020-12-22 10:30:44 +0100

    fix steps when trying to login

--- server/server.js
+++ server/server.js
@@ -1,27 +1,30 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 5000;
-const cors = require("cors");
-const mysql = require("mysql");
-const bodyParser = require("body-parser");
-const users = require("./users.js");
+const cors = require('cors');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const users = require('./users.js');
 const user = require('./user.js');
 
 const con = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Test12345@",
-  database: "Matcha",
+  host: 'localhost',
+  user: 'root',
+  password: 'Test12345@',
+  database: 'Matcha',
 });
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use("/users", users);
+app.use('/users', users);
 app.use('/user', user);
-app.get("/image/:image", (req, res) => {
+app.get('/image/:image', (req, res) => {
   res.sendFile(`${__dirname}/${req.params.image}`);
+});
+app.get('/images/:image', (req, res) => {
+  res.sendFile(`${__dirname}/images/${req.params.image}`);
 });
 app.listen(port, () => {
   console.log(`Matcha server app listening at http://localhost:${port}`);
